1ee6425f5955a31c9dfecda236776a1efdcbc716
Luis Hernandez <luis.hernandezv0803@gmail.com>
2022-01-14 16:57:31 -0300

    Ajustando permisologia

--- application/views/orders/listOrders.php
+++ application/views/orders/listOrders.php
@@ -85,11 +85,20 @@
                         $disabled = '';
                       }
                       
-                      echo "<td><button type='button' class='btn btn-outline-success btn-rounded waves-effect' title='Ver Orden' data-toggle='modal' data-target='#seeOrder' data-id=".$value['id']."><i class='far fa-eye' aria-hidden='true'></i></button>
-                      <button type='button' class='btn btn-outline-info btn-rounded waves-effect ml-1' title='Editar Orden' data-toggle='modal' data-target='#updateOrder' data-id=".$value['idOrder']."><i class='far fa-edit' aria-hidden='true'></i></button>
-                      <button type='button' class='btn btn-outline-danger btn-rounded waves-effect ml-1' title='Eliminar Orden' data-toggle='modal' data-target='#delOrder' data-id=".$value['idOrder']."><i class='far fa-trash-alt' aria-hidden='true'></i></button>
-                      <button type='button' class='btn btn-outline-primary btn-rounded waves-effect ml-1' $disabled title='Generar Certificado' data-toggle='modal' data-target='#genCertificado' data-id=".$value['idOrder']."><i class='fas fa-file-signature' aria-hidden='true'></i></button></td>";
-                      echo "</tr>";
+                      echo "<td>";
+                      if ($session['previewInfo']) {
+                        echo "<button type='button' class='btn btn-outline-success btn-rounded waves-effect' title='Ver Orden' data-toggle='modal' data-target='#seeOrder' data-id=".$value['id']."><i class='far fa-eye' aria-hidden='true'></i></button>";
+                      }
+                      if ($session['editInfo']) {
+                        echo "<button type='button' class='btn btn-outline-info btn-rounded waves-effect ml-1' title='Editar Orden' data-toggle='modal' data-target='#updateOrder' data-id=".$value['idOrder']."><i class='far fa-edit' aria-hidden='true'></i></button>";
+                      }
+                      if ($session['deleteInfo']) {
+                        echo "<button type='button' class='btn btn-outline-danger btn-rounded waves-effect ml-1' title='Eliminar Orden' data-toggle='modal' data-target='#delOrder' data-id=".$value['idOrder']."><i class='far fa-trash-alt' aria-hidden='true'></i></button>";
+                      }
+                      if ($session['generateCert']) {
+                        echo "<button type='button' class='btn btn-outline-primary btn-rounded waves-effect ml-1' $disabled title='Generar Certificado' data-toggle='modal' data-target='#genCertificado' data-id=".$value['idOrder']."><i class='fas fa-file-signature' aria-hidden='true'></i></button>";
+                      }
+                        echo "</td></tr>";
                     }
                   }
                   ?>
