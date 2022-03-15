520b2ca42c44e1e5abe291ea36f1753dcdeaf183
Luis Hernandez <luis.hernandezv0803@gmail.com>
2022-03-15 19:46:45 -0300

    update lang portuguese

--- application/language/spanish/login_lang.php
+++ application/language/spanish/login_lang.php
@@ -4,18 +4,34 @@
  * Spanish language
  */
 
-$lang['label_username']                 = 'Usuario'; 
-$lang['placeholder_username']           = 'Ingresar Usuario...'; 
-$lang['label_password']                 = 'Contraseña'; 
-$lang['placeholder_password']           = 'Ingresar Contraseña...'; 
-$lang['btn_login']                      = 'Ingresar'; 
-$lang['empty_user_pwd']                 = 'Usuario y Contraseña vacias'; 
-$lang['empty_user']                     = 'Campo Usuario vacio'; 
-$lang['empty_pwd']                      = 'Campo Contraseña vacias'; 
+// $lang['label_username']                 = 'Usuario'; 
+// $lang['placeholder_username']           = 'Ingresar Usuario...'; 
+// $lang['label_password']                 = 'Contraseña'; 
+// $lang['placeholder_password']           = 'Ingresar Contraseña...'; 
+// $lang['btn_login']                      = 'Ingresar'; 
+// $lang['empty_user_pwd']                 = 'Usuario y Contraseña vacias'; 
+// $lang['empty_user']                     = 'Campo Usuario vacio'; 
+// $lang['empty_pwd']                      = 'Campo Contraseña vacias'; 
 
-$lang['alert_adm']                      = 'Usuario No Asignado a Personal. Contacte al Admistrador'; 
-$lang['alert_error_user_pwd']           = 'Usuario Inhabilitado'; 
+// $lang['alert_adm']                      = 'Usuario No Asignado a Personal. Contacte al Admistrador'; 
+// $lang['alert_error_user_pwd']           = 'Usuario Inhabilitado'; 
+
+// $lang['copyright']                      = 'Copyright';
+// $lang['name_system']                    = 'Administración de Certificados';
+// $lang['all_rights_reserved']            = 'Todos los derechos reservados';
+
+$lang['label_username']                 = 'Usuário'; 
+$lang['placeholder_username']           = 'Digite Usuário...'; 
+$lang['label_password']                 = 'Senha'; 
+$lang['placeholder_password']           = 'Digite a senha...'; 
+$lang['btn_login']                      = 'Entrar'; 
+$lang['empty_user_pwd']                 = 'Nome de usuário e senha vazios'; 
+$lang['empty_user']                     = 'Campo de usuário vazio'; 
+$lang['empty_pwd']                      = 'Campo de senha vazio'; 
+
+$lang['alert_adm']                      = 'Usuário não atribuído a Pessoal. Entre em contato com o administrador'; 
+$lang['alert_error_user_pwd']           = 'Usuário desativado'; 
 
 $lang['copyright']                      = 'Copyright';
-$lang['name_system']                    = 'Administración de Certificados';
-$lang['all_rights_reserved']            = 'Todos los derechos reservados';
+$lang['name_system']                    = 'Sistema de Gerenciamento de Certificados';
+$lang['all_rights_reserved']            = 'Todos os direitos reservados';
